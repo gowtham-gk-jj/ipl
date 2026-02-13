103007696e45c78b507923d0c83d66bc54aaf987
Your Name <ssaggowtham@gmail.com>
2026-02-13 13:28:25 +0530

    first commit

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,18 +1,20 @@
 const jwt = require("jsonwebtoken");
 
 const protect = (req, res, next) => {
-  const token = req.headers.authorization;
+  const authHeader = req.headers.authorization;
 
-  if (!token) {
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ message: "Not authorized" });
   }
 
   try {
+    const token = authHeader.split(" ")[1]; // 👈 REMOVE "Bearer "
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
     req.user = decoded;
     next();
   } catch (error) {
-    res.status(401).json({ message: "Invalid token" });
+    return res.status(401).json({ message: "Invalid token" });
   }
 };
 
